27e5ef7adb0f2ac0ec76cda6b202a9b5ef349e17
Jalani <jalani2727@gmail.com>
2026-03-15 18:36:27 -0400

    Add detailed hand-typed notes about what promises are

--- pt8/load-image.js
+++ pt8/load-image.js
@@ -1,3 +1,14 @@
+// Here is a function that contains logic that will be returned as a Promise. 
+// The logic inside is what is executed each time your promise object is called in your code
+
+// The <new Promise()> syntax is how you construct your promise. Constructing a promise in this way turns your logic into a promise object that has access to promise methods and also gives it some internal properties called the state and the result which can't be directly accessed.
+
+// Your logic will contain a pathway to use the resolve method or to use the reject method. In this example, we reach the resolve method if the image loads successfully and reject if there is an error.
+
+// The "state" property of your promise will update to "fulfilled" if the onload event is triggered - aka the image loads successfully. It  will update to "rejected" if the onerror event is triggered - aka there is some error with the image. 
+
+// If you never call resolve or reject, the promise status will never update from "pending" and will not proceed, leaving your program hanging.
+
 function loadImage(url) {
   return new Promise((resolve, reject) => {
     let image = new Image()
@@ -7,7 +18,7 @@ function loadImage(url) {
     }
 
     image.onerror = function() {
-      let message =
+      let msg =
         'Could not load image at ' + url
       reject(new Error(msg))
     }
@@ -17,3 +28,10 @@ function loadImage(url) {
   })
 }
 export default loadImage
+
+// Outiside of this construction of your promise, you'll actually call this function to create your Promise object and use it in your program. 
+// When you call it, you will attached your "consumer methods" to consume the result of your promise.
+// .then() will accept a callback function that will run if the state of your promise is fulfilled - AKA your logic to call resolve() was hit.
+// .catch() will accept a callback function that will run if the state of your promise is rejected - AKA your logic to call reject() was hit.
+
+// Optionally, you can call finally on your promise to clean up anything that you may want to stop. Like stopping a loading icon or something.
--- pt8/promises.js
+++ pt8/promises.js
@@ -1,8 +1,8 @@
-// In this series, there have been a lot of techniques that involve callback functions.
+// In this series, there have been a lot of techniques that involve callback functions. They are way of telling your code, when this function is done, do this.
 
-// They are way of telling your code, when this function is done, do this.
+// Promises do this as well. But you should think of a promise as a "contract" of sorts. With a promise, you're assuming that something will be ready in the future via some function.
 
-// Promises do this as well. But you should think of a promise as a "contract" of sorts. With a promise, you're assuming that something will be ready in the future via some function
+// But why use one? Well in a real-world application, creating functions to render your content could lead to a smoother User experience. Rather than having content trickle in randomly, you could have it all come it at once when it's ready or only load content when certain conditions are met. Maybe you only want to show images when a user scrolls to some gallery section. Or you want to wait until an api call is finished before doing something. 
 
 // Basic Promise example
 import loadImage from './load-image'
@@ -14,13 +14,26 @@ let addImg = (src) => {
   document.body.appendChild(imgElement)
 }
 
-Promise.all([
+Promise.allSettled([ // This method is useful here because we have a bunch of promises that we want to execute and wait for. The all() method is part of the "Promise API" and is a way to turn all of the promise we want to create into a single promise that will execute them all at the same time (in parallel) rather than calling them one after another (sequentially). 
   loadImage('images/cat1.jpg'),
   loadImage('images/cat2.jpg'),
   loadImage('images/cat3.jpg'),
   loadImage('images/cat4.jpg')
 ]).then((images) => {
-  images.forEach(img => addImg(img.src))
+  images.forEach((result, index) => {
+    // need something to happen when status is "fulfilled"
+    // need something to happen when status is "rejected"
+    // Use result.status to set the src via addImg
+    if (result.status === "fulfilled"){
+        addImg(result.value.src) // Promise.allSettled provides the result structure that would indicate that you would use result.value - Console logging this would reveal it but just try to remember this.
+    } else {
+        console.log(`Image ${index} failed: `, result.reason) // Promise.allSettled provides the result structure that would indicate that you would use result.reason - Console logging this would reveal it but just try to remember this.
+    }
+  })
 }).catch((error) => {
-  // handle error later
+  console.log('This is the msg variable defined in the construction of your loadImage promise: ', error);
 })
+
+// Think about this: Currently, if one image errors out, none of the images load. How could this be improved to show the images that had sucessful promises?
+
+//Answer: switching Promise.all() to Promise.allSettled() and enhancing the logic for what should happen in the .then()
