66c3bf28d19ed581622727d7a29738831691dce0
Jalani <jalani2727@gmail.com>
2026-03-16 15:14:45 -0400

    Finalize functor notes

--- pt9/functors.js
+++ pt9/functors.js
@@ -1,8 +1,10 @@
 // My understanding of functors after watch "#10 and #11" of hhis Functional Programming series
 
 
-// Functors are types of functions that accept a value and a map method in order to execute some logic. This is a useful concept because there may be cases in which there is a lot of unneccessary logic found in a particular function you've created and you want to separate come concerns.
+// Functors are containers that accept values of any type and have a map method in order to execute some logic. 
 
 // The goal of using a functor is to return some new object of the same type that contains transformed values without altering the orginal.
 
-// A Promise is also a functor for this reason. The .then() method that you call on your promise acts as the .map() in that it takes your promise or array of promises, applies a function to the future result (data).
+// An array is a functor for this reason.
+
+// A Promise is also a functor for this reason. The .then() method that you call on your promise acts as the .map() in that it takes your promise or array of promises, applies a  function to the future result (data).
